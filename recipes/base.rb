54710ec723f007f0dd15952b6a032bad4cb6cc05
Max V <maxim.valiarovski@target.com>
2015-02-01 00:11:41 -0600

    small stuff

--- recipes/base.rb
+++ recipes/base.rb
@@ -3,6 +3,7 @@ include_recipe 'dnsmasq'
 #include_recipe 'chef-client'
 my_ipaddress = "#{node['test_template']['hostip']}"
 
+node.set['consul']['advertise_interface'] = 'eth1'
 node.set['consul']['client_address'] = '0.0.0.0'
 node.set['consul']['bind_addr'] = node['test_template']['hostip']
 node.set['consul']['advertise_addr'] = node['test_template']['hostip']
